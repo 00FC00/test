ded203b0b31593006fbbd63cd55584e74dcabd4f
哲思 <595240129@qq.com>
2023-10-24 17:35:58 +0800

    初始化

---
+++ Sources/MyLibrary/MyLibrary.swift
@@ -0,0 +1,10 @@
+public struct MyLibrary {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+    
+    public static func log() {
+        print("FC的日志打印")
+    }
+}
